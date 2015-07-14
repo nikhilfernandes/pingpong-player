f13b632b618066fd5483fa6b01dc156854bbc646
Nikhil <fernandes.nikhil@gmail.com>
2015-07-14 12:55:17 +0530

    list championships on player page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,10 @@
+require "open-uri"
 class HomeController < ApplicationController
 
   def index
+    @data = URI.parse("http://127.0.0.1:3000/championships").read    
+    @championships = JSON.parse(@data)
+    puts @championships
     
   end
 
--- app/models/http_request.rb
+++ app/models/http_request.rb
@@ -1,3 +1,6 @@
+require "net/http"
+require "uri"
+
 class HttpRequest
 
   def self.post(host, port, path, params, auth_token)
@@ -6,6 +9,12 @@ class HttpRequest
     payload = JSON.generate params
     request = Net::HTTP::Post.new(path, headers)    
     res = http.request(request, payload)
+  end
+
+  def self.get(host, port, path)
+    headers = { 'Content-Type' => 'application/json', 'accept' => 'application/json'}
+    http = Net::HTTP.new(host, port)    
+    res = http.get(path)
   end
 
 end
