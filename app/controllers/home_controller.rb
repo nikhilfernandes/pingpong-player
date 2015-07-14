013b653eb79b87b513d1d115d9f8f7491ac48abc
Nikhil <fernandes.nikhil@gmail.com>
2015-07-14 21:27:29 +0530

    list championships in a table

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,10 +3,8 @@ class HomeController < ApplicationController
 
   def index
     @data = URI.parse("http://127.0.0.1:3000/championships").read    
-    @championships = JSON.parse(@data)
-    puts @championships
-    @player_info = PlayerInfo.new(PLAYER_INFO["player"])
-    
+    @championships = JSON.parse(@data)    
+    @player_info = PlayerInfo.new(PLAYER_INFO["player"])    
   end
 
 end
